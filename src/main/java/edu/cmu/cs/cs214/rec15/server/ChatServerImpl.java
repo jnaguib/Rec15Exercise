a9e2b71f493133ff0ecba23a22248cfd2f0f908b
unknown <stariq@CLR-1032-13.qatar.win.cmu.edu>
2015-11-26 15:53:31 +0300

    time stamp addition done and tested

--- src/main/java/edu/cmu/cs/cs214/rec15/server/ChatServerImpl.java
+++ src/main/java/edu/cmu/cs/cs214/rec15/server/ChatServerImpl.java
@@ -7,6 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -184,6 +185,8 @@ public class ChatServerImpl extends Thread implements ChatServer {
             // Synchronize because we are iterating through all clients in a
             // thread
             synchronized (clients) {
+            	Date receivedAt = new Date();
+				msg.setServerTimestamp(receivedAt);
                 for (Socket s : clients) {
                     try {
                         ObjectOutputStream out = new ObjectOutputStream(
